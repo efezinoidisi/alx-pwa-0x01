1ebfbcdd4ade17fc4205d73a2f92c1b6b2b66a9b
efezino <enduranceidisi13@gmail.com>
2025-01-24 18:24:24 +0100

    add movies page

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'm.media-amazon.com',
+        pathname: '/**',
+        protocol: 'https',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
